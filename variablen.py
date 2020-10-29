781b699d92fd43f98648e98b5f39404461135737
Ben Stähli <bnzk@bnzk.ch>
2020-10-29 11:06:00 +0100

    variablen uff

---
+++ variablen.py
@@ -0,0 +1,28 @@
+
+
+print "Hello!"
+
+
+a = 10
+b = 20
+
+print a + b
+print type(a)
+
+
+a = "10"
+b = "20"
+
+c = a + b
+print c
+print type(c)
+
+
+# ein kommentar ist das
+c = int(c)
+d = 5
+
+print c + d
+
+print type(c)
+
